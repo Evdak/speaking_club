9c9a6ca01599c06e5b772de87a7fa0d08f26d225
Evdokimov Kirill <breed.kir.evd@ya.ru>
2023-09-12 23:02:59 +0300

    fix

--- speaking_club/apps/individual_lessons/helpers.py
+++ speaking_club/apps/individual_lessons/helpers.py
@@ -18,9 +18,11 @@ def create_meeting(student: IndividualStudent, lesson: IndividualLesson):
         "account_id": student.teacher.zoom_key,
         "client_secret": student.teacher.zoom_sec,
     }
-    response = requests.post(auth_token_url,
-                             auth=(student.teacher.zoom_client, student.teacher.zoom_sec),
-                             data=data)
+    response = requests.post(
+        auth_token_url,
+        auth=(student.teacher.zoom_client, student.teacher.zoom_sec),
+        data=data,
+    )
 
     if response.status_code != 200:
         print("Unable to get access token")
@@ -29,18 +31,18 @@ def create_meeting(student: IndividualStudent, lesson: IndividualLesson):
 
     headers = {
         "Authorization": f"Bearer {access_token}",
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
     }
     payload = {
         "topic": topic,
         "duration": duration,
-        'start_time': f'{start_date}T10:{start_time}',
-        "type": 2
+        "start_time": f"{start_date}T{start_time}",
+        "type": 2,
     }
 
-    resp = requests.post(f"{api_base_url}/users/me/meetings",
-                         headers=headers,
-                         json=payload)
+    resp = requests.post(
+        f"{api_base_url}/users/me/meetings", headers=headers, json=payload
+    )
 
     if resp.status_code != 201:
         print("Unable to generate meeting link")
@@ -55,7 +57,7 @@ def create_meeting(student: IndividualStudent, lesson: IndividualLesson):
         "purpose": response_data["topic"],
         "duration": response_data["duration"],
         "message": "Success",
-        "status": 1
+        "status": 1,
     }
 
     return content
