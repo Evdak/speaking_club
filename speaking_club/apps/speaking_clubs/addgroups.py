6b0385e01d0b2d2b9b1289a9b8cda640f201f6b5
Evdak <57111036+Evdak@users.noreply.github.com>
2024-01-16 15:50:06 +0300

    new groups

---
+++ speaking_club/apps/speaking_clubs/addgroups.py
@@ -0,0 +1,430 @@
+from speaking_clubs.models import Chat, Stream, Group, Level
+
+
+def main():
+    levels = ["A1", "A2", "B1"]
+    times = ["18", "19", "20"]
+    weeks = ["Понедельник - Суббота", "Понедельник - Четверг"]
+
+    ch_num = 0
+    gr_num = 0
+
+    reser = Stream.objects.filter(name="Занято").first()
+    cur_stream = Stream.objects.filter(name="20 поток").first()
+
+    if not all((reser, cur_stream)):
+        print(f"{reser=} {cur_stream=}")
+        return
+
+    for chat in Chat.objects.all():
+        chat.stream = reser
+        chat.save()
+
+    for level in levels:
+        for time in times:
+            for week in weeks:
+                if level == "A1" and time == "18" and week == "Понедельник - Четверг":
+                    gr = Group.objects.filter(
+                        level__name=level, weekdays=week, time=time
+                    ).first()
+                    if not gr:
+                        print(f"not gr {level} {time} {week}")
+                        return
+                    gr_num += 1
+                    urls = [
+                        "https://t.me/+pxCN_-Mlyj0wOWMy",
+                        "https://t.me/+7tooEEHH-_dmNDQy",
+                        "https://t.me/+_VujuWPCV3s1Njhi",
+                        "https://t.me/+z_7m8z0X7gJhMDcy",
+                    ]
+                    for url in urls:
+                        ch = Chat.objects.filter(chat=url).first()
+                        if not ch:
+                            print(f"not ch '{level}' '{time}' '{week}' '{url}'")
+                        if ch:
+                            ch.stream = None
+                            ch.group = gr
+                            ch.save()
+                            ch_num += 1
+
+                if level == "A1" and time == "18" and week == "Понедельник - Суббота":
+                    gr = Group.objects.filter(
+                        level__name=level, weekdays=week, time=time
+                    ).first()
+                    if not gr:
+                        print(f"not gr {level} {time} {week}")
+                        return
+                    gr_num += 1
+                    urls = [
+                        "https://t.me/+U71OPhCVIq8wZmM6",
+                        "https://t.me/+V3hyl727fvxjYTMy",
+                        "https://t.me/+Wtdc7WcAVrM1YjNi",
+                    ]
+                    for url in urls:
+                        ch = Chat.objects.filter(chat=url).first()
+                        if not ch:
+                            print(f"not ch '{level}' '{time}' '{week}' '{url}'")
+                        if ch:
+                            ch.stream = None
+                            ch.group = gr
+                            ch.save()
+                            ch_num += 1
+
+                if level == "A1" and time == "19" and week == "Понедельник - Четверг":
+                    gr = Group.objects.filter(
+                        level__name=level, weekdays=week, time=time
+                    ).first()
+                    if not gr:
+                        print(f"not gr {level} {time} {week}")
+                        return
+                    gr_num += 1
+                    urls = [
+                        "https://t.me/+beb50e_PTpNjYjZi",
+                        "https://t.me/+DmzlCcJ7jZRmYjUy",
+                        "https://t.me/+vgYepv4R0lNlYzEy",
+                    ]
+                    for url in urls:
+                        ch = Chat.objects.filter(chat=url).first()
+                        if not ch:
+                            print(f"not ch '{level}' '{time}' '{week}' '{url}'")
+                        if ch:
+                            ch.stream = None
+                            ch.group = gr
+                            ch.save()
+                            ch_num += 1
+                if level == "A1" and time == "19" and week == "Понедельник - Суббота":
+                    gr = Group.objects.filter(
+                        level__name=level, weekdays=week, time=time
+                    ).first()
+                    if not gr:
+                        print(f"not gr {level} {time} {week}")
+                        return
+                    gr_num += 1
+                    urls = [
+                        "https://t.me/+7YB5EOziIXNjMTIy",
+                        "https://t.me/+twVdCJeq7Vk2YjRi",
+                        "https://t.me/+J45yW2I_pSw2Njhi",
+                        "https://t.me/+vpYF6usMJck3ZTI6",
+                    ]
+                    for url in urls:
+                        ch = Chat.objects.filter(chat=url).first()
+                        if not ch:
+                            print(f"not ch '{level}' '{time}' '{week}' '{url}'")
+                        if ch:
+                            ch.stream = None
+                            ch.group = gr
+                            ch.save()
+                            ch_num += 1
+                if level == "A1" and time == "20" and week == "Понедельник - Четверг":
+                    gr = Group.objects.filter(
+                        level__name=level, weekdays=week, time=time
+                    ).first()
+                    if not gr:
+                        print(f"not gr {level} {time} {week}")
+                        return
+                    gr_num += 1
+                    urls = [
+                        "https://t.me/+KTDjG3m-x3FlMGRi",
+                        "https://t.me/+buexGPPbtd05NjRi",
+                        "https://t.me/+4UPEltoTJEg2YWZi",
+                        "https://t.me/+bAkEtQx5gIg0MTUy",
+                    ]
+                    for url in urls:
+                        ch = Chat.objects.filter(chat=url).first()
+                        if not ch:
+                            print(f"not ch '{level}' '{time}' '{week}' '{url}'")
+                        if ch:
+                            ch.stream = None
+                            ch.group = gr
+                            ch.save()
+                            ch_num += 1
+
+                if level == "A1" and time == "20" and week == "Понедельник - Суббота":
+                    gr = Group.objects.filter(
+                        level__name=level, weekdays=week, time=time
+                    ).first()
+                    if not gr:
+                        print(f"not gr {level} {time} {week}")
+                        return
+                    gr_num += 1
+                    urls = [
+                        "https://t.me/+dJjhjuOFssQ2MmMy",
+                        "https://t.me/+8ERMFQwDQl04Zjli",
+                        "https://t.me/+29Kt73vSRldjZTE6",
+                    ]
+                    for url in urls:
+                        ch = Chat.objects.filter(chat=url).first()
+                        if not ch:
+                            print(f"not ch '{level}' '{time}' '{week}' '{url}'")
+                        if ch:
+                            ch.stream = None
+                            ch.group = gr
+                            ch.save()
+                            ch_num += 1
+
+                if level == "A2" and time == "18" and week == "Понедельник - Четверг":
+                    gr = Group.objects.filter(
+                        level__name=level, weekdays=week, time=time
+                    ).first()
+                    if not gr:
+                        print(f"not gr {level} {time} {week}")
+                        return
+                    gr_num += 1
+                    urls = [
+                        "https://t.me/+43D6sW5W6Uo4MjAy",
+                        "https://t.me/+h2RBCStzPd1jZTJi",
+                        "https://t.me/+lvgpvq-tmbE4ZDUy",
+                    ]
+                    for url in urls:
+                        ch = Chat.objects.filter(chat=url).first()
+                        if not ch:
+                            print(f"not ch '{level}' '{time}' '{week}' '{url}'")
+                        if ch:
+                            ch.stream = None
+                            ch.group = gr
+                            ch.save()
+                            ch_num += 1
+
+                if level == "A2" and time == "18" and week == "Понедельник - Суббота":
+                    gr = Group.objects.filter(
+                        level__name=level, weekdays=week, time=time
+                    ).first()
+                    if not gr:
+                        print(f"not gr {level} {time} {week}")
+                        return
+                    gr_num += 1
+                    urls = [
+                        "https://t.me/+sLIE2tdeomVlODli",
+                        "https://t.me/+1CPJAjeffcRkYzEy",
+                        "https://t.me/+DP9fjc3xDtJhZTM6",
+                    ]
+                    for url in urls:
+                        ch = Chat.objects.filter(chat=url).first()
+                        if not ch:
+                            print(f"not ch '{level}' '{time}' '{week}' '{url}'")
+                        if ch:
+                            ch.stream = None
+                            ch.group = gr
+                            ch.save()
+                            ch_num += 1
+
+                if level == "A2" and time == "19" and week == "Понедельник - Четверг":
+                    gr = Group.objects.filter(
+                        level__name=level, weekdays=week, time=time
+                    ).first()
+                    if not gr:
+                        print(f"not gr {level} {time} {week}")
+                        return
+                    gr_num += 1
+                    urls = [
+                        "https://t.me/+VjoH6wH3_CxjZGRi",
+                        "https://t.me/+Ak_H2qD_1UI4ODYy",
+                        "https://t.me/+7m_zxHtF8UgxYjYy",
+                    ]
+                    for url in urls:
+                        ch = Chat.objects.filter(chat=url).first()
+                        if not ch:
+                            print(f"not ch '{level}' '{time}' '{week}' '{url}'")
+                        if ch:
+                            ch.stream = None
+                            ch.group = gr
+                            ch.save()
+                            ch_num += 1
+                if level == "A2" and time == "19" and week == "Понедельник - Суббота":
+                    gr = Group.objects.filter(
+                        level__name=level, weekdays=week, time=time
+                    ).first()
+                    if not gr:
+                        print(f"not gr {level} {time} {week}")
+                        return
+                    gr_num += 1
+                    urls = [
+                        "https://t.me/+0Y4sXPoOwr40Yzcy",
+                        "https://t.me/+USdyqxnU9IlhYzgy",
+                        "https://t.me/+WuoicPRiSY0zNDNi",
+                        "https://t.me/+h3AwkZZ8qiFhMjMy",
+                    ]
+                    for url in urls:
+                        ch = Chat.objects.filter(chat=url).first()
+                        if not ch:
+                            print(f"not ch '{level}' '{time}' '{week}' '{url}'")
+                        if ch:
+                            ch.stream = None
+                            ch.group = gr
+                            ch.save()
+                            ch_num += 1
+
+                if level == "A2" and time == "20" and week == "Понедельник - Четверг":
+                    gr = Group.objects.filter(
+                        level__name=level, weekdays=week, time=time
+                    ).first()
+                    if not gr:
+                        print(f"not gr {level} {time} {week}")
+                        return
+                    gr_num += 1
+                    urls = [
+                        "https://t.me/+WcNnBaFUK80zM2Zi",
+                        "https://t.me/+5s9dW4NUmdliMmQ6",
+                        "https://t.me/+8TNlfe5HGTI2MzAy",
+                    ]
+                    for url in urls:
+                        ch = Chat.objects.filter(chat=url).first()
+                        if not ch:
+                            print(f"not ch '{level}' '{time}' '{week}' '{url}'")
+                        if ch:
+                            ch.stream = None
+                            ch.group = gr
+                            ch.save()
+                            ch_num += 1
+
+                if level == "A2" and time == "20" and week == "Понедельник - Суббота":
+                    gr = Group.objects.filter(
+                        level__name=level, weekdays=week, time=time
+                    ).first()
+                    if not gr:
+                        print(f"not gr {level} {time} {week}")
+                        return
+                    gr_num += 1
+                    urls = [
+                        "https://t.me/+KQ8drg-M0RUyN2Qy",
+                        "https://t.me/+Gpq5_69flpdjMDBi",
+                    ]
+                    for url in urls:
+                        ch = Chat.objects.filter(chat=url).first()
+                        if not ch:
+                            print(f"not ch '{level}' '{time}' '{week}' '{url}'")
+                        if ch:
+                            ch.stream = None
+                            ch.group = gr
+                            ch.save()
+                            ch_num += 1
+                if level == "B1" and time == "18" and week == "Понедельник - Четверг":
+                    gr = Group.objects.filter(
+                        level__name=level, weekdays=week, time=time
+                    ).first()
+                    if not gr:
+                        print(f"not gr {level} {time} {week}")
+                        return
+                    gr_num += 1
+                    urls = [
+                        "https://t.me/+_tHqpsvwGedhYzFi",
+                        "https://t.me/+cB0TFI5v3Os2YjMy",
+                    ]
+                    for url in urls:
+                        ch = Chat.objects.filter(chat=url).first()
+                        if not ch:
+                            print(f"not ch '{level}' '{time}' '{week}' '{url}'")
+                        if ch:
+                            ch.stream = None
+                            ch.group = gr
+                            ch.save()
+                            ch_num += 1
+                if level == "B1" and time == "18" and week == "Понедельник - Суббота":
+                    gr = Group.objects.filter(
+                        level__name=level, weekdays=week, time=time
+                    ).first()
+                    if not gr:
+                        print(f"not gr {level} {time} {week}")
+                        return
+                    gr_num += 1
+                    urls = [
+                        "https://t.me/+sB1pb4ZTtRdjN2Ey",
+                        "https://t.me/+GyuC7L5Ax71iNmMy",
+                    ]
+                    for url in urls:
+                        ch = Chat.objects.filter(chat=url).first()
+                        if not ch:
+                            print(f"not ch '{level}' '{time}' '{week}' '{url}'")
+                        if ch:
+                            ch.stream = None
+                            ch.group = gr
+                            ch.save()
+                            ch_num += 1
+                if level == "B1" and time == "19" and week == "Понедельник - Четверг":
+                    gr = Group.objects.filter(
+                        level__name=level, weekdays=week, time=time
+                    ).first()
+                    if not gr:
+                        print(f"not gr {level} {time} {week}")
+                        return
+                    gr_num += 1
+                    urls = [
+                        "https://t.me/+meSo3Y9HFZ1iYTcy",
+                        "https://t.me/+_IQzWirqEmFmZTg6",
+                    ]
+                    for url in urls:
+                        ch = Chat.objects.filter(chat=url).first()
+                        if not ch:
+                            print(f"not ch '{level}' '{time}' '{week}' '{url}'")
+                        if ch:
+                            ch.stream = None
+                            ch.group = gr
+                            ch.save()
+                            ch_num += 1
+                if level == "B1" and time == "19" and week == "Понедельник - Суббота":
+                    gr = Group.objects.filter(
+                        level__name=level, weekdays=week, time=time
+                    ).first()
+                    if not gr:
+                        print(f"not gr {level} {time} {week}")
+                        return
+                    gr_num += 1
+                    urls = [
+                        "https://t.me/+BI4kGYd-9tNjMGEy",
+                        "https://t.me/+jFad35g9cdA2NzQ6",
+                    ]
+                    for url in urls:
+                        ch = Chat.objects.filter(chat=url).first()
+                        if not ch:
+                            print(f"not ch '{level}' '{time}' '{week}' '{url}'")
+                        if ch:
+                            ch.stream = None
+                            ch.group = gr
+                            ch.save()
+                            ch_num += 1
+                if level == "B1" and time == "20" and week == "Понедельник - Четверг":
+                    gr = Group.objects.filter(
+                        level__name=level, weekdays=week, time=time
+                    ).first()
+                    if not gr:
+                        print(f"not gr {level} {time} {week}")
+                        return
+                    gr_num += 1
+                    urls = [
+                        "https://t.me/+TDWWXzRQhOo1NmJi",
+                        "https://t.me/+Dt-X0HQYvz02ZTAy",
+                    ]
+                    for url in urls:
+                        ch = Chat.objects.filter(chat=url).first()
+                        if not ch:
+                            print(f"not ch '{level}' '{time}' '{week}' '{url}'")
+                        if ch:
+                            ch.stream = None
+                            ch.group = gr
+                            ch.save()
+                            ch_num += 1
+                if level == "B1" and time == "20" and week == "Понедельник - Суббота":
+                    gr = Group.objects.filter(
+                        level__name=level, weekdays=week, time=time
+                    ).first()
+                    if not gr:
+                        print(f"not gr {level} {time} {week}")
+                        return
+                    gr_num += 1
+                    urls = [
+                        "https://t.me/+EzVXTuMs2iZkZWUy",
+                        "https://t.me/+B5dcHp2vlfgxMjRi",
+                    ]
+                    for url in urls:
+                        ch = Chat.objects.filter(chat=url).first()
+                        if not ch:
+                            print(f"not ch '{level}' '{time}' '{week}' '{url}'")
+                        if ch:
+                            ch.stream = None
+                            ch.group = gr
+                            ch.save()
+                            ch_num += 1
+    print(f"{ch_num=} {gr_num=}")
+
+
+main()
